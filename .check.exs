232151276df8621fe3566a854e638699bb071f90
bcksl <bcksl@proton.me>
2023-06-15 06:06:44 +0300

    chore: add boilerplate

---
+++ .check.exs
@@ -0,0 +1,8 @@
+[
+  tools: [
+    outdated: "mix hex.outdated",
+    deps_audit: "mix deps.audit",
+    sobelow: "mix sobelow --config",
+    coveralls: "mix coveralls"
+  ]
+]
---
+++ config/config.exs
@@ -0,0 +1,20 @@
+import Config
+
+if Mix.env() == :dev do
+  config :git_hooks,
+    hooks: [
+      pre_commit: [
+        tasks: [
+          {:cmd, "mix check"}
+        ]
+      ]
+    ]
+
+  config :git_ops,
+    mix_project: Mix.Project.get!(),
+    changelog_file: "CHANGELOG.md",
+    repository_url: "https://github.com/bcksl/ash_geo",
+    manage_mix_version?: true,
+    manage_readme_version: ["README.md"],
+    version_tag_prefix: "v"
+end
